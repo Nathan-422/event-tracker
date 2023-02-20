cbf5ab6edb72b60077a43fecb71213f897b8b1cf
Nathan-422 <natiouswright@gmail.com>
2023-02-20 14:40:28 -0600

    Part way through model building

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,10 +1,13 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,15 +16,11 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+
+
     @GetMapping
     public String displayAllEvents(Model model) {
-
-//        TODO: code this
-
-        List<String> events = new ArrayList<>();
-
-
-
+        model.addAttribute("events", EventData.getEvents());
         return "events/index";
     }
 
@@ -30,9 +29,14 @@ public class EventController {
         return "events/create";
     }
 
-    @PostMapping
-    public String addToEventList() {
-
+    @PostMapping("create")
+    public String createEvent(
+            @RequestParam String eventName,
+            @RequestParam String eventDescription,
+            Model model)
+    {
+        EventData.add(new Event(eventName, eventDescription));
+        return "redirect:";
     }
 
 }
